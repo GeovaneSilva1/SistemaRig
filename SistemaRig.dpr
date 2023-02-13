c966135d7c891e989bd526e91fd404f8603c28da
Qualidade Visual 18 <qualvisual18@sci10.local>
2023-02-12 21:56:25 -0300

    executável atualizado

--- SistemaRig.dpr
+++ SistemaRig.dpr
@@ -5,7 +5,8 @@ uses
   uViewPdv in 'src\view\uViewPdv.pas' {frViewPdv},
   uViewLogin in 'src\view\uViewLogin.pas' {frPageLogin},
   dmControl in 'src\model\dmControl.pas' {frdmControl: TDataModule},
-  uViewPagamentos in 'src\view\uViewPagamentos.pas' {frViewPagamentos};
+  uViewPagamentos in 'src\view\uViewPagamentos.pas' {frViewPagamentos},
+  uViewPagePix in 'src\view\uViewPagePix.pas' {framePix: TFrame};
 
 {$R *.res}
 
