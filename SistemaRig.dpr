8d69629242f8fef19d515a75a25d0098e903df9f
Qualidade Visual 18 <qualvisual18@sci10.local>
2023-02-12 21:50:59 -0300

    projeto e executável atualizados

--- SistemaRig.dpr
+++ SistemaRig.dpr
@@ -4,7 +4,8 @@ uses
   Vcl.Forms,
   uViewPdv in 'src\view\uViewPdv.pas' {frViewPdv},
   uViewLogin in 'src\view\uViewLogin.pas' {frPageLogin},
-  dmControl in 'src\model\dmControl.pas' {frdmControl: TDataModule};
+  dmControl in 'src\model\dmControl.pas' {frdmControl: TDataModule},
+  uViewPagamentos in 'src\view\uViewPagamentos.pas' {frViewPagamentos};
 
 {$R *.res}
 
@@ -13,5 +14,6 @@ begin
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TfrViewPdv, frViewPdv);
   Application.CreateForm(TfrdmControl, frdmControl);
+  Application.CreateForm(TfrViewPagamentos, frViewPagamentos);
   Application.Run;
 end.
